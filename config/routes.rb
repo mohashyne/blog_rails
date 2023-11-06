9a28c9ba30e08a2899bdc65f42cc9c82c43b9e79
MSA <msa@localhost.localdomain>
2023-11-06 16:36:20 -0700

    route and controller refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  # API routes are versioned under /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
