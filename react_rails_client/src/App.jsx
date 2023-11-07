b74d038db520fe573d55acf3503b095b4fb36c0b
MSA <crystalbluetech@gmail.com>
2023-11-07 11:45:45 +0100

    Added vite_react_blog client App

---
+++ react_rails_client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import PostsList from "./features/posts/PostsList";
+
+function App() {
+
+
+  return (
+    <>
+    <div className='app'>
+      <h1>React on Rails Blog</h1>
+      <p>Find this application layout in client/src/app.jsx</p>
+      
+      <PostsList />
+      
+    </div>
+    </>
+  )
+}
+
+export default App
